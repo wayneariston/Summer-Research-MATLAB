b887a61e05f16d6372983f86588e983a4735d591
wayneariston <nwfariston@cmc.pshs.edu.ph>
2022-06-05 12:41:47 -0400

    updates

--- Test_1.m
+++ Test_1.m
@@ -2,12 +2,14 @@ tic
 image_length = 128;
 image_height = 128;
 atom_diameter = 8;
-lambda = 0.6; % [atom^-1]
+lambda = 3; % [rad atom^-1]
+% lambda should be <<1, but not too small as well
+lambda = lambda*2*pi/atom_diameter;
 
 Q_x = [1 0];
 Q_y = [-Q_x(2) Q_x(1)]; % just to ensure orthogonality
-Q_x = Q_x * 2*pi / (norm(Q_x)*atom_diameter);
-Q_y = Q_y * 2*pi / (norm(Q_y)*atom_diameter);
+Q_x = Q_x * 2*pi / (norm(Q_x)*atom_diameter); % [rad atom^-1]
+Q_y = Q_y * 2*pi / (norm(Q_y)*atom_diameter); % [rad atom^-1]
 
 inputs = zeros(6);
 a = 0;
@@ -25,13 +27,13 @@ while a<=10
     lattice1 = normies(uTransform(u,Q_x,Q_y))*atom_diameter/2;
 
     % calculate for the vector field using the Lawler-Fujita algorithm
-
-    ucalc = lawlerFujita(lattice1,Q_x,Q_y,lambda/atom_diameter);
-    [meanErr(uint16(a*100)+1), stdErr, ouah] = uCompare(u,ucalc,lambda/atom_diameter);
+    ucalc = lawlerFujita(lattice1,Q_x,Q_y,lambda);
+    [meanErr(uint16(a*100)+1), stdErr, ouah] = uCompare(u,ucalc,lambda);
     a = a + 0.01;
 end
 toc
 
+figure;
 plot(0:0.01:double(uint8(a)-0.01),meanErr);
 xlabel("max value of distortion [atom diameter]");
 ylabel("relative error");
@@ -42,8 +44,8 @@ meanErr = zeros(1);
 while a<=10
     u = uCreate(image_height,image_length,atom_diameter,[0 a],[0 0],[0 0]);
     lattice1 = normies(uTransform(u,Q_x,Q_y))*atom_diameter/2;
-    ucalc = lawlerFujita(lattice1,Q_x,Q_y,lambda/atom_diameter);
-    [meanErr(uint16(a*100)+1), stdErr, ouah] = uCompare(u,ucalc,lambda/atom_diameter);
+    ucalc = lawlerFujita(lattice1,Q_x,Q_y,lambda);
+    [meanErr(uint16(a*100)+1), stdErr, ouah] = uCompare(u,ucalc,lambda);
     a = a + 0.01;
 end
 toc
@@ -59,8 +61,8 @@ meanErr = zeros(1);
 while a<=10
     u = uCreate(image_height,image_length,atom_diameter,[0 0],[a 0],[0 0]);
     lattice1 = normies(uTransform(u,Q_x,Q_y))*atom_diameter/2;
-    ucalc = lawlerFujita(lattice1,Q_x,Q_y,lambda/atom_diameter);
-    [meanErr(uint16(a*100)+1), stdErr, ouah] = uCompare(u,ucalc,lambda/atom_diameter);
+    ucalc = lawlerFujita(lattice1,Q_x,Q_y,lambda);
+    [meanErr(uint16(a*100)+1), stdErr, ouah] = uCompare(u,ucalc,lambda);
     a = a + 0.01;
 end
 toc
@@ -75,8 +77,8 @@ meanErr = zeros(1);
 while a<=10
     u = uCreate(image_height,image_length,atom_diameter,[0 0],[0 a],[0 0]);
     lattice1 = normies(uTransform(u,Q_x,Q_y))*atom_diameter/2;
-    ucalc = lawlerFujita(lattice1,Q_x,Q_y,lambda/atom_diameter);
-    [meanErr(uint16(a*100)+1), stdErr, ouah] = uCompare(u,ucalc,lambda/atom_diameter);
+    ucalc = lawlerFujita(lattice1,Q_x,Q_y,lambda);
+    [meanErr(uint16(a*100)+1), stdErr, ouah] = uCompare(u,ucalc,lambda);
     a = a + 0.01;
 end
 toc
@@ -91,8 +93,8 @@ meanErr = zeros(1);
 while a<=10
     u = uCreate(image_height,image_length,atom_diameter,[0 0],[0 0],[a 0]);
     lattice1 = normies(uTransform(u,Q_x,Q_y))*atom_diameter/2;
-    ucalc = lawlerFujita(lattice1,Q_x,Q_y,lambda/atom_diameter);
-    [meanErr(uint16(a*100)+1), stdErr, ouah] = uCompare(u,ucalc,lambda/atom_diameter);
+    ucalc = lawlerFujita(lattice1,Q_x,Q_y,lambda);
+    [meanErr(uint16(a*100)+1), stdErr, ouah] = uCompare(u,ucalc,lambda);
     a = a + 0.01;
 end
 toc
@@ -107,8 +109,8 @@ meanErr = zeros(1);
 while a<=10
     u = uCreate(image_height,image_length,atom_diameter,[0 0],[0 0],[0 a]);
     lattice1 = normies(uTransform(u,Q_x,Q_y))*atom_diameter/2;
-    ucalc = lawlerFujita(lattice1,Q_x,Q_y,lambda/atom_diameter);
-    [meanErr(uint16(a*100)+1), stdErr, ouah] = uCompare(u,ucalc,lambda/atom_diameter);
+    ucalc = lawlerFujita(lattice1,Q_x,Q_y,lambda);
+    [meanErr(uint16(a*100)+1), stdErr, ouah] = uCompare(u,ucalc,lambda);
     a = a + 0.01;
 end
 toc
@@ -118,3 +120,4 @@ xlabel("max value of distortion [atom diameter]");
 ylabel("relative error");
 
 legend("drift x", "drift y", "hysteresis x", "hysteresis y", "creep x", "creep y");
+title("$\Lambda_u=$" + lambda,"Interpreter","latex");
